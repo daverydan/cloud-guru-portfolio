b410d8f150143d0a7343d99ee5924dc259d190c7
Danny Avery <danny.w.avery@gmail.com>
2017-09-23 09:00:04 -0600

    Move static Markup for work examples into React Components

--- js/example-work.js
+++ js/example-work.js
@@ -3,7 +3,37 @@ import React from 'react';
 class ExampleWork extends React.Component {
 	render() {
 		return(
-			<section>It works!</section>
+			<section className="section section--alignCentered section--description">
+
+				{ this.props.work.map( (example, idx) => {
+					return(
+				  		<ExampleWorkProject example={example} key={idx} />
+			  		)
+				  })
+				}
+
+		    </section>
+		)
+	}
+}
+
+class ExampleWorkProject extends React.Component {
+	render() {
+		let example = this.props.example;
+		return(
+			<div className="section__exampleWrapper">
+			  <div className="section__example">
+			    <img alt={ example.image.desc }
+			         className="section__exampleImage"
+			         src={ example.image.src }/>
+			    <dl className="color--cloud">
+			      <dt className="section__exampleTitle section__text--centered">
+			        { example.title }
+			      </dt>
+			      <dd></dd>
+			    </dl>
+			  </div>
+			</div>
 		)
 	}
 }
